3a306972e63db43d9c9b943891f8ba08b75dd2bc
amillerlite <ansley257@gmail.com>
2023-08-02 18:35:42 -0400

    task-bar add
    added task bars in, realized that i couldn't click through them, moved some stuff, now they're beneath the background and I can't get them to overlay.

--- src/app/components/gantt-chart/gantt-chart/gantt-chart.component.ts
+++ src/app/components/gantt-chart/gantt-chart/gantt-chart.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 import { TasksService } from 'src/app/services/tasks.service';
 import { Task } from 'src/app/interfaces/task.model';
 import { TaskDatesService } from 'src/app/services/task-dates.service';
+import { TaskHeightInPx } from 'src/app/global-constants';
 
 @Component({
   selector: 'app-gantt-chart',
@@ -14,6 +15,7 @@ export class GanttChartComponent {
   public startDate!: Date;
   public endDate!: Date;
   public numberOfTasks!: number;
+  public height: number = TaskHeightInPx;
 
   constructor(
     public tasksService: TasksService,
@@ -23,7 +25,10 @@ export class GanttChartComponent {
   ngOnInit(): void {
     this.tasksService.getTasks().subscribe((tasks) => {
       this.tasks = tasks;
-      console.log('tasks', this.tasks);
+      this.tasks.forEach((task) => {
+        task.startDate = new Date(task.startDate);
+        task.endDate = new Date(task.endDate);
+      });
       this.startDate = this.taskDatesService.getStartDate(this.tasks);
       this.endDate = this.taskDatesService.getEndDate(this.tasks);
       this.numberOfTasks = this.tasksService.getNumberOfTasks(this.tasks);
